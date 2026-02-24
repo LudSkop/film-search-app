e78748b393cf0ecc8d763c666e8832f7aecf3c4e
LudSkop <149317914+LudSkop@users.noreply.github.com>
2026-02-24 22:45:13 +0000

    Deploying to gh-pages from @ LudSkop/film-search-app@aa8804a596f6606fde1dd7dc8b19c2faaa9c0f25 🚀

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import{a as c}from"./assets/vendor-BJ9gahTP.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const a="https://api.themoviedb.org/3/",l="trending/movie/week",u=void 0,d=document.querySelector(".js-movie-list");document.querySelector(".js-load-more");const f=1;async function p(){return(await c.get(`${a}${l}`,{params:{api_key:u,page:f}})).data}p().then(o=>{console.log(o),d.insertAdjacentHTML("beforeend",m(o.results))}).catch(o=>{console.log(o)});function m(o){const r="https://image.tmdb.org/t/p/w500";return o.map(({original_title:s,vote_average:n,poster_path:e,release_date:t})=>`
+import{a as p}from"./assets/vendor-BJ9gahTP.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const f="https://api.themoviedb.org/3/",m="trending/movie/week",g=void 0,l=document.querySelector(".js-movie-list"),a=document.querySelector(".js-load-more");a.addEventListener("click",h);let i=1;async function d(r=1){return(await p.get(`${f}${m}`,{params:{api_key:g,page:r}})).data}d().then(r=>{console.log(r),l.insertAdjacentHTML("beforeend",u(r.results)),r.page<r.total_pages&&a.classList.replace("load-more-hidden","js-load-more")}).catch(r=>{console.log(r)});function u(r){const o="https://image.tmdb.org/t/p/w500";return r.map(({original_title:s,vote_average:n,poster_path:e,release_date:t})=>`
     <li class="movie-card">
-        <img src="${r}${e}" alt="${s}"/>
+        <img src="${o}${e}" alt="${s}"/>
         <div class="movie-info">
         <h2>${s}</h2>
-        <p>Release Date :${t}</p>
-        <p>vote Average :${n}</p>
+        <p>Release Date: ${t}</p>
+        <p>vote Average: ${n}</p>
         </div>
     </li>
-    `).join("")}
+    `).join("")}async function h(r){i++;try{console.log("кількість кліків: ",i);const o=await d(i);console.log(o),l.insertAdjacentHTML("beforeend",u(o.results)),o.page>=o.total_pages&&a.classList.replace("js-load-more","load-more-hidden")}catch(o){alert(o.messag)}}
 //# sourceMappingURL=index.js.map
